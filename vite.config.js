cf92f68aeba7ce80e06dde15ab6fb292d1a4a866
ShaikhMaaz <maaz@example.com>
2025-10-28 17:29:00 +0530

    Initial commit — MCI frontend

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ✅ must match your repo name on GitHub
 export default defineConfig({
-  base: '/mci-frontend/',   // <--- set to "/REPO_NAME/"
+  base: '/mci-frontend/',
   plugins: [react()],
 })
